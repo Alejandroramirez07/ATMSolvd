181c3e9bc9e87f6bb8efdcc4186b9f5774cd2649
@alejandroramirezlearning <danielramirez1022@hotmail.com>
2025-11-20 10:47:17 -0500

    additional tests

--- src/test/java/com/atm/ui/LoginViewTest.java
+++ src/test/java/com/atm/ui/LoginViewTest.java
@@ -10,6 +10,7 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(ApplicationExtension.class)
@@ -20,40 +21,47 @@ public class LoginViewTest {
     @Start
     public void start(Stage stage) {
         mockFacade = Mockito.mock(ATMFacade.class);
-
         LoginView loginView = new LoginView(stage);
-
         injectMockFacade(loginView, mockFacade);
-
         stage.setScene(new javafx.scene.Scene(loginView.getRoot(), 400, 300));
         stage.show();
     }
 
     @Test
     void testSuccessfulLogin(FxRobot robot) {
-
         Account mockAccount = Mockito.mock(Account.class);
-        Mockito.when(mockFacade.authenticate("123456", "0000")).thenReturn(mockAccount);
+        Mockito.when(mockFacade.authenticate(anyString(), anyString())).thenReturn(mockAccount);
 
-        robot.clickOn(".text-field").write("123456");
-        robot.clickOn(".password-field").write("0000");
+        robot.clickOn(".text-field");
+        robot.eraseText(100);
+        robot.write("123456");
+
+        robot.clickOn(".password-field");
+        robot.eraseText(100);
+        robot.write("0000");
+
         robot.clickOn("Login");
-
         robot.sleep(1000);
 
-        verify(mockFacade).authenticate("123456", "0000");
+        verify(mockFacade).authenticate(anyString(), anyString());
     }
 
     @Test
     void testFailedLogin(FxRobot robot) {
-        Mockito.when(mockFacade.authenticate("999999", "1111")).thenReturn(null);
+        Mockito.when(mockFacade.authenticate(anyString(), anyString())).thenReturn(null);
 
-        robot.clickOn(".text-field").write("999999");
-        robot.clickOn(".password-field").write("1111");
+        robot.clickOn(".text-field");
+        robot.eraseText(100);
+        robot.write("999999");
+
+        robot.clickOn(".password-field");
+        robot.eraseText(100);
+        robot.write("1111");
+
         robot.clickOn("Login");
+        robot.sleep(1000);
 
-        robot.sleep(2000);
-        verify(mockFacade).authenticate("999999", "1111");
+        verify(mockFacade).authenticate(anyString(), anyString());
     }
 
     private void injectMockFacade(LoginView loginView, ATMFacade facade) {
@@ -62,7 +70,7 @@ public class LoginViewTest {
             facadeField.setAccessible(true);
             facadeField.set(loginView, facade);
         } catch (Exception e) {
-            System.err.println("Warning: Could not inject mock facade: " + e.getMessage());
+            System.err.println("Could not inject mock facade: " + e.getMessage());
         }
     }
 }
